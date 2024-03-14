02e188f14d240138db7874cd61f73df91514ca41
Ted M Lin <tedmlin@gmail.com>
2024-03-14 12:03:26 -0400

    Add about modal

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,6 +2,7 @@
 import { Ref, ref } from 'vue';
 import { JsonButtonConfig } from '../App.vue';
 import { ControllerType } from '../controller';
+import Modal from './Modal.vue';
 
 
 defineEmits<{
@@ -17,13 +18,39 @@ const jsonPayload: Ref<JsonButtonConfig> = ref({
   mapping: [
   ]
 })
+
+const isAboutVisible: Ref<boolean> = ref(false)
 </script>
 
 <template>
   <div class="print:hidden flex flex-row gap-1 mb-4 bg-slate-400 px-2">
     <button class="rounded border-2 p-1" @click="$emit('loadJsonFile', 'testing')">load preset</button>
     <button class="rounded border-2 p-1" @click="$emit('loadJson', jsonPayload)">load data</button>
+    <div class="grow"></div>
+    <button class="rounded border-2 p-1" @click="isAboutVisible = true">about</button>
   </div>
+
+  <Modal v-show="isAboutVisible" @close="isAboutVisible = false" mask-close close-button>
+    <template v-slot:header>About</template>
+    <template v-slot:body>
+      <p>Welcome to the <span class="font-medium">Controller Map</span>!</p>
+      <p>This is a work-in-progress, with some areas more unfinished than others.</p>
+      <p>The primary purpose of this tool is to generate a printable map of controller buttons.</p>
+      <h3 class="mt-4 font-bold">Credits</h3>
+      <p>Thanks to the following people/projects:</p>
+      <ul class="list-disc list-inside mx-2">
+        <li>XBox Series Button Icons and Controls were created by <a class="underline hover:decoration-blue-400"
+            target="_blank" href="https://zacksly.itch.io">Zacksly (Licensed under CC BY 3.0)</a></li>
+      </ul>
+      <div class="mt-4 flex flex-row justify-center space-x-10">
+        <div>
+          <a class="underline hover:decoration-blue-400" target="_blank"
+            href="https://github.com/kitlaan/controller-map"><font-awesome-icon
+              icon="fa-brands fa-github" />&nbsp;github</a>
+        </div>
+      </div>
+    </template>
+  </Modal>
 </template>
 
 <style scoped>
---
+++ src/components/Modal.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+
+defineProps<{
+  maskClose?: boolean
+  closeButton?: boolean
+}>()
+
+defineEmits<{
+  (e: 'close'): void
+}>()
+</script>
+
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop" @click.self="maskClose && $emit('close')">
+      <div class="modal-container">
+        <div class="modal-header">
+          <slot name="header">
+          </slot>
+        </div>
+
+        <div class="modal-body">
+          <slot name="body">
+          </slot>
+        </div>
+
+        <div class="modal-footer">
+          <slot name="footer">
+            <div class="modal-buttons">
+              <button class="rounded border-2 px-4 hover:bg-gray-200" v-show="closeButton" @click="$emit('close')">Close</button>
+            </div>
+          </slot>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style scoped>
+  .modal-fade-enter,
+  .modal-fade-leave-to {
+    opacity: 0;
+  }
+
+  .modal-fade-enter-active,
+  .modal-fade-leave-active {
+    transition: opacity 0.5s ease;
+  }
+
+  .modal-backdrop {
+    position: fixed;
+    z-index: 99;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal-container {
+    background: white;
+    box-shadow: 0 2px 8px;
+    border-radius: 2px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+    min-width: 200px;
+    max-width: 70vw;
+  }
+
+  .modal-header,
+  .modal-footer {
+    padding: 8px 15px;
+    display: flex;
+  }
+
+  .modal-header {
+    position: relative;
+    border-bottom: 1px solid #eee;
+    color: #4aae98;
+    font-weight: bold;
+    justify-content: space-between;
+  }
+
+  .modal-footer {
+    border-top: 1px solid #eee;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
+
+  .modal-body {
+    position: relative;
+    padding: 8px 15px;
+    max-height: 70vh;
+    overflow-y: auto;
+  }
+
+  .modal-buttons {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+</style>
